06f53374ea0c865f5cc8619eca209e8abd53d83f
Thomas <hollandpirates@gmail.com>
2017-10-30 17:32:21 +0100

    fix link to profile

--- texlive_install.sh
+++ texlive_install.sh
@@ -16,85 +16,16 @@ if ! command -v texlua > /dev/null; then
   cd install-tl-20*
 
   # Install a minimal system
-  ./install-tl --profile=../support/texlive.profile
+  ./install-tl --profile=texlive.profile
 
   cd ..
 fi
 
-# Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
-
-# The test framework itself
-tlmgr install l3build
-
-# Required to build plain and LaTeX formats:
-# TeX90 plain for unpacking, pdfLaTeX, LuaLaTeX and XeTeX for tests
-tlmgr install cm etex knuth-lib latex-bin tex tex-ini-files unicode-data \
-  xetex
-
-# Additional requirements for (u)pLaTeX, done with no dependencies to
-# avoid large font payloads
-tlmgr install --no-depends babel ptex uptex ptex-base uptex-base ptex-fonts \
-  uptex-fonts platex uplatex
-
-# Assuming a 'basic' font set up, metafont is required to avoid
-# warnings with some packages and errors with others
-tlmgr install metafont mfware
-
-
-# Set up graphics: nowadays split over a few places and requiring
-# HO's bundle
-tlmgr install graphics graphics-cfg graphics-def oberdiek
-
-# Contrib packages for testing
-#
-# The packages themselves are done with --no-depends to avoid
-# picking up l3kernel, etc.
-#
-# fontspec comes first as other packages tested have it as a dep
-tlmgr install --no-depends fontspec
-tlmgr install ifluatex lm lualibs luaotfload
-
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# Dependencies other than the core l3build set up, metafont, fontspec and the
-# 'graphics stack' (itself needed by fontspec) are listed below
-tlmgr install --no-depends \
-  chemformula \
-  ctex        \
-  mhchem      \
-  siunitx     \
-  unicode-math
-tlmgr install --no-depends cjk
 tlmgr install   \
-  adobemapping  \
   amsfonts      \
-  amsmath       \
-  chemgreek     \
-  cjkpunct      \
-  ctablestack   \
-  ec            \
-  environ       \
-  etoolbox      \
-  fandol        \
-  filehook      \
-  ifxetex       \
-  lm-math       \
-  lualatex-math \
-  luatexbase    \
-  luatexja      \
-  ms            \
-  pgf           \
-  tools         \
-  trimspaces    \
-  ucharcat      \
-  ulem          \
-  units         \
-  xcolor        \
-  xecjk         \
-  xkeyval       \
-  xunicode      \
-  zhmetrics     \
-  zhnumber
+  stmaryrd      \
+  amsmath
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
