d4270e20242d232f3eeaa3788dfcabc2c9d649d4
Thomas <hollandpirates@gmail.com>
2017-10-30 19:34:07 +0100

    test texlua cache check and add exam package

--- texlive_install.sh
+++ texlive_install.sh
@@ -21,9 +21,14 @@ if ! command -v texlua > /dev/null; then
   cd ..
 fi
 
+# Just including texlua so the cache check above works
+# Needed for any use of texlua even if not testing LuaTeX
+tlmgr install luatex
+
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
 tlmgr install   \
+  exam          \
   amsfonts      \
   stmaryrd      \
   amsmath
